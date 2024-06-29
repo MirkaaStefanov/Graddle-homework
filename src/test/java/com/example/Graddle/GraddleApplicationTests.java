adc9020601262559dd403368b6b2b29c6c6529a1
MirkaaStefanov <mirkanstefanov2007@gmail.com>
2024-06-30 01:40:46 +0300

    changed test

--- src/test/java/com/example/Graddle/GraddleApplicationTests.java
+++ src/test/java/com/example/Graddle/GraddleApplicationTests.java
@@ -6,10 +6,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GraddleApplicationTests {
 
-	@Test
-	void contextLoads() {
-		// This method is intended to ensure the Spring application context loads successfully.
-		// No further implementation is necessary for this test.
-		throw new UnsupportedOperationException("Not yet implemented");
-	}
 }
