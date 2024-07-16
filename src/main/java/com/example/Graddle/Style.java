9a76f3795f2597d1de71bc1ad94b7739eb27ef59
Eri1905 <ery.mtl1905@gmail.com>
2024-07-16 12:17:20 +0300

    change

--- src/main/java/com/example/Graddle/Style.java
+++ src/main/java/com/example/Graddle/Style.java
@@ -4,6 +4,7 @@ public class Style {
 
     private String name;
     private String identifier;
+    private int number;
 
     public Style(String name, String identifier) {
         this.name = name;
@@ -24,5 +25,13 @@ public class Style {
 
     public void setIdentifier(String identifier) {
         this.identifier = identifier;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
     }
 }
